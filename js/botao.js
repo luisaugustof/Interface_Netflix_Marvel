4c11b9d0a4877782ea898ef39e377bed632e79cf
Bruxo <luis12augusto@gmail.com>
2022-04-21 17:22:04 -0300

    Projeto recriar a interface da netflix

---
+++ js/botao.js
@@ -0,0 +1,11 @@
+function assistir(){
+
+    //abrir link em uma nova aba
+    window.open ("https://www.youtube.com/watch?v=PJza3ZaFeAU&t");
+   
+    //abrir na guia de origem
+    // window.location.href = "https://www.youtube.com/watch?v=PJza3ZaFeAU&t";
+}
+function maisinfo(){
+    window.location.href = "maisinformacoes.html";
+}
